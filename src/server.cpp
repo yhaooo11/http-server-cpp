5404bba16340a508e5eff56777a3150568533153
yifanhao <yifanhao5@gmail.com>
2024-07-31 18:45:55 -0700

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -11,6 +11,7 @@
 #include <sstream>
 #include <map>
 #include <thread>
+#include <fstream>
 
 
 
@@ -83,7 +84,7 @@ HTTPRequest parse_request(std::string request) {
     return req;
 }
 
-int handle_client(int client_fd, struct sockaddr_in client_addr) {
+int handle_client(int client_fd, struct sockaddr_in client_addr, std::string dir) {
   char buff[2048];
   int client_addr_len = sizeof(client_addr);
   // use ssize_t if return value can be negative (i.e. error value)
@@ -111,17 +112,19 @@ int handle_client(int client_fd, struct sockaddr_in client_addr) {
       std::string subStr = request.path.substr(6);
       response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(subStr.length()) + "\r\n\r\n" + subStr;
     } else if (request.path.substr(0, 7) == "/files/") {
-        // std::string directory = ".";
-        // std::string filename = request.path.substr(7);
-        // std::ifstream file(directory + "/" + filename);
-        // if (!file) {
-        //     HTTPResponse response = { "HTTP/1.1 404 Not Found", "text/plain", {}, "Not Found" };
-        //     write_response(client_fd, response);
-        //     return;
-        // }
-        // std::stringstream body;
-        // body << file.rdbuf();
-        // HTTPResponse response = { "HTTP/1.1 200 OK", "application/octet-stream", { {"Content-Length", std::to_string(body.str().length())} }, body.str() };
+      std::string directory = dir;
+      std::string filename = request.path.substr(7);
+      std::ifstream file(directory + filename);
+      std::cout << directory + filename << "\n";
+      if (!file) {
+        response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n";
+        send(client_fd , response.c_str() , response.length(), 0);
+        return 0;
+      }
+      std::stringstream body;
+      body << file.rdbuf();
+      //  HTTPResponse response = { "HTTP/1.1 200 OK", "application/octet-stream", { {"Content-Length", std::to_string(body.str().length())} }, body.str() };
+      response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + std::to_string(body.str().length()) + "\r\n\r\n" + body.str();
         // write_response(client_fd, response);
     } else {
       response = "HTTP/1.1 404 Not Found\r\n\r\n";
@@ -138,6 +141,17 @@ int handle_client(int client_fd, struct sockaddr_in client_addr) {
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
+      std::cout << "Number of arguments: " << argc << std::endl;
+
+    // Print each command-line argument
+    for (int i = 0; i < argc; ++i) {
+        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+    }
+
+  std::string dir = "";
+  if (argc > 1) {
+    dir = argv[2];
+  }
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   
@@ -190,7 +204,7 @@ int main(int argc, char **argv) {
   while (true) {
     client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
     std::cout << "Client connected\n" << client_fd << "\n";
-    std::thread th(handle_client, client_fd, client_addr);
+    std::thread th(handle_client, client_fd, client_addr, dir);
     th.detach();
   }
   
