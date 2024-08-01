8e18e449c601dc9cc9818e4df78b90194026db7d
yifanhao <yifanhao5@gmail.com>
2024-07-31 18:35:36 -0700

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -10,6 +10,8 @@
 #include <vector>
 #include <sstream>
 #include <map>
+#include <thread>
+
 
 
 std::vector<std::string> split_string(const std::string &str, const std::string &delimiter) {
@@ -81,6 +83,59 @@ HTTPRequest parse_request(std::string request) {
     return req;
 }
 
+int handle_client(int client_fd, struct sockaddr_in client_addr) {
+  char buff[2048];
+  int client_addr_len = sizeof(client_addr);
+  // use ssize_t if return value can be negative (i.e. error value)
+  // reading from file descriptor here, another way is just int ret = read(client_fd, buffer, sizeof(buffer));
+  // use recv for TCP, recvfrom for UDP (UDP is connectionless)
+  ssize_t t = recv(client_fd, buff, sizeof(buff) - 1, 0);
+  if (t < 0){
+    std::cerr << "listen failed\n";
+    return 1;
+  }
+
+  std::string r(buff);
+  std::cout << client_fd << "\n";
+  std::cout << r << "\n";
+  HTTPRequest request = parse_request(r);
+
+  std::string response;
+  if (request.method == "GET") {
+    if (request.path == "/") {
+      response = "HTTP/1.1 200 OK\r\n\r\n";
+    } else if (request.path == "/user-agent") {
+      std::string body = request.headers["User-Agent"];
+      response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(body.length()) + "\r\n\r\n" + body;
+    } else if (request.path.substr(0, 6) == "/echo/") {
+      std::string subStr = request.path.substr(6);
+      response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(subStr.length()) + "\r\n\r\n" + subStr;
+    } else if (request.path.substr(0, 7) == "/files/") {
+        // std::string directory = ".";
+        // std::string filename = request.path.substr(7);
+        // std::ifstream file(directory + "/" + filename);
+        // if (!file) {
+        //     HTTPResponse response = { "HTTP/1.1 404 Not Found", "text/plain", {}, "Not Found" };
+        //     write_response(client_fd, response);
+        //     return;
+        // }
+        // std::stringstream body;
+        // body << file.rdbuf();
+        // HTTPResponse response = { "HTTP/1.1 200 OK", "application/octet-stream", { {"Content-Length", std::to_string(body.str().length())} }, body.str() };
+        // write_response(client_fd, response);
+    } else {
+      response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+  } else {
+      // HTTPResponse response = { "HTTP/1.1 405 Method Not Allowed", "text/plain", {}, "Method Not Allowed" };
+      // write_response(client_fd, response);
+  }
+
+  send(client_fd , response.c_str() , response.length(), 0);
+
+  return 0;
+}
+
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
@@ -126,58 +181,18 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  if (client_fd < 0) {
-    std::cerr << "accept failed\n";
-    return 1;
+  // int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  // if (client_fd < 0) {
+  //   std::cerr << "accept failed\n";
+  //   return 1;
+  // }
+  int client_fd;
+  while (true) {
+    client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    std::cout << "Client connected\n" << client_fd << "\n";
+    std::thread th(handle_client, client_fd, client_addr);
+    th.detach();
   }
-
-  std::cout << "Client connected\n";
-
-  char buff[2048];
-  // use ssize_t if return value can be negative (i.e. error value)
-  // reading from file descriptor here, another way is just int ret = read(client_fd, buffer, sizeof(buffer));
-  ssize_t t = recvfrom(client_fd, buff, sizeof(buff) - 1, 0, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  if (t < 0){
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-
-  std::string r(buff);
-  HTTPRequest request = parse_request(r);
-
-  std::string response;
-  if (request.method == "GET") {
-    if (request.path == "/") {
-      response = "HTTP/1.1 200 OK\r\n\r\n";
-    } else if (request.path == "/user-agent") {
-      std::string body = request.headers["User-Agent"];
-      response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(body.length()) + "\r\n\r\n" + body;
-    } else if (request.path.substr(0, 6) == "/echo/") {
-      std::string subStr = request.path.substr(6);
-      response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(subStr.length()) + "\r\n\r\n" + subStr;
-    } else if (request.path.substr(0, 7) == "/files/") {
-        // std::string directory = ".";
-        // std::string filename = request.path.substr(7);
-        // std::ifstream file(directory + "/" + filename);
-        // if (!file) {
-        //     HTTPResponse response = { "HTTP/1.1 404 Not Found", "text/plain", {}, "Not Found" };
-        //     write_response(client_fd, response);
-        //     return;
-        // }
-        // std::stringstream body;
-        // body << file.rdbuf();
-        // HTTPResponse response = { "HTTP/1.1 200 OK", "application/octet-stream", { {"Content-Length", std::to_string(body.str().length())} }, body.str() };
-        // write_response(client_fd, response);
-    } else {
-      response = "HTTP/1.1 404 Not Found\r\n\r\n";
-    }
-  } else {
-      // HTTPResponse response = { "HTTP/1.1 405 Method Not Allowed", "text/plain", {}, "Method Not Allowed" };
-      // write_response(client_fd, response);
-  }
-
-  send(client_fd , response.c_str() , response.length(), 0);
   
   close(server_fd);
 
